6d8fe972e7532772b6850983394efec9af39788a
BooleanTrue <cang34gs@gmail.com>
2024-06-01 19:21:20 +0300

    bug fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 const search = document.querySelector(".search-box button");
 const weatherBox = document.querySelector(".weather-box");
-const weatherDetails = document.querySelector(".weather-detais");
+const weatherDetails = document.querySelector(".weather-details");
 const error404 = document.querySelector(".not-found");
 const cityHide = document.querySelector(".city-hide");
 
@@ -11,23 +11,21 @@ search.addEventListener("click", () => {
 
   if (city == "") return;
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${{APIKey}"
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      if (json.cod == "404") {
-        cityHide.textContent = city;
-        container.style.height = "404px";
-        weatherBox.classList.remove("active");
-        weatherDetails.classList.remove("active");
-        error404.classList.add("active");
-        return;
-      }
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+  ).then(response => response.json()).then(json => {
+    if (json.cod == "404") {
+      cityHide.textContent = city;
+      container.style.height = "404px";
+      weatherBox.classList.remove("active");
+      weatherDetails.classList.remove("active");
+      error404.classList.add("active");
+      return;
+    }
 
-      container.style.height = "555px";
-      weatherBox.classList.add("active");
-      weatherDetails.classList.add("active");
-      error404.classList.remove("active");
+    container.style.height = "555px";
+    weatherBox.classList.add("active");
+    weatherDetails.classList.add("active");
+    error404.classList.remove("active");
 
       const image = document.querySelector(".weather-box img");
       const temperature = document.querySelector(".weather-box .temperature");
@@ -48,9 +46,6 @@ search.addEventListener("click", () => {
         weatherDetails.classList.add("active");
         error404.classList.remove("active");
 
-        setTimeout(() => {
-          container.classList.remove("active");
-        }, 2500);
 
         switch (json.weather[0].main) {
           case "Clear":
@@ -87,52 +82,3 @@ search.addEventListener("click", () => {
       }
     });
 });
-
-const infoWeather = document.querySelector(".info-weather");
-const infoHumidity = document.querySelector(".info-humidity");
-const infoWind = document.querySelector(".info-wind");
-
-const elCloneInfoWeather = infoWeather.cloneNode(true);
-const elCloneInfoHumidity = infoHumidity.cloneNode(true);
-const elCloneInfoWind = infoWind.cloneNode(true);
-
-elCloneInfoWeather.id = "clone-info-weather";
-elCloneInfoWeather.classList.add("active-clone");
-
-elCloneInfoHumidity.id = "clone-info-humidity";
-elCloneInfoHumidity.classList.add("active-clone");
-
-elCloneInfoWind.id = "clone-info-wind";
-elCloneInfoWind.classList.add("active-clone");
-
-setTimeout(() => {
-  infoWeather.insertAdjacentElement("afterend", elCloneInfoWeather);
-  infoHumidity.insertAdjacentElement("afterend", elCloneInfoHumidity);
-  infoWind.insertAdjacentElement("afterend", elCloneInfoWind);
-}, 2200);
-
-const cloneInfoWeather = document.querySelectorAll(
-  ".info-weather.active-clone"
-);
-const totalCloneInfoWeatherFirst = cloneInfoWeather.length;
-const cloneInfoWeatherFirst = cloneInfoWeather[0];
-
-const cloneInfoHumidity = document.querySelectorAll(
-  ".info-humidity.active-clone"
-);
-const cloneInfoHumidityFirst = cloneInfoHumidity[0];
-
-const cloneInfoWind = document.querySelectorAll(".info-wind.active-clone");
-const cloneInfoWindFirst = cloneInfoWind[0];
-
-if (totalCloneInfoWeather > 0) {
-  cloneInfoWeatherFirst.classList.remove("active-clone");
-  cloneInfoHumidityFirst.classList.remove("active-clone");
-  cloneInfoWindFirst.classList.remove("active-clone");
-
-  setTimeout(() => {
-    cloneInfoWeatherFirst.remove();
-    cloneInfoHumidityFirst.remove();
-    cloneInfoWindFirst.remove();
-  }, 2200);
-}
